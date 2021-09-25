f9a2944264b3b59836d85d2781975b04b4dc0c33
Payal Sasmal <sasmalpayal@gmail.com>
2021-09-25 12:03:36 +0530

    array worked

--- array.py
+++
@@ -1,6 +0,0 @@
-import array
-
-ara = array('i',[7,8,9,1])
-
-for i in range(0,4):
-    print(ara[i])
---
+++ arrays.py
@@ -0,0 +1,6 @@
+from array import *
+
+ara = array('i', [7, 8, 9, 1])
+
+for i in range(0,4):
+    print(ara[i])
