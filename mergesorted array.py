8140e2b94a8c2d3a1404915907fcb4143dd8fabb
Payal Sasmal <sasmalpayal@gmail.com>
2022-01-26 23:03:09 +0530

    updated 3 ways to solve it

--- mergesorted array.py
+++ mergesorted array.py
@@ -1,4 +1,8 @@
-class Solution:
+# https://leetcode.com/problems/merge-sorted-array/
+
+
+# 1st way using return and modifying everything
+class Solution1:
     def mergeSortedarray(nums1, m, nums2, n):
         newList1 = []
         newList2 = []
@@ -11,7 +15,36 @@ class Solution:
         mergeList.sort()
         return mergeList
 
-s = Solution
+s = Solution1
 print(s.mergeSortedarray([1,2,3,0,0,0],3,[2,5,6],3))
+
+# 2nd way using return function
+class Solution2:
+    def mergeSortedarray(nums1, m, nums2, n):
+        newList1 = []
+        newList2 = []
+        
+        mergeList = nums1[:m] + nums2[:n]
+        mergeList.sort()
+        return mergeList
+
+s = Solution2
+print(s.mergeSortedarray([1,2,3,0,0,0],3,[2,5,6],3))
+
+#3rd way to solve it. without return and without extra changes only modifed nums1
+class Solution3:
+    def mergeSortedarray(nums1, m, nums2, n):
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        del nums1[m:]
+        nums1.extend(nums2[:n])
+        nums1.sort()
+        return nums1
+
+s = Solution2
+print(s.mergeSortedarray([1,2,3,0,0,0],3,[2,5,6],3))
+
+
 
 
