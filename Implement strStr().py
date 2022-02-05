ff62c39ad8ba1095c109b88e3d68e3822658813e
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-05 23:09:47 +0530

    Implement strStr() solved

--- Implement strStr().py
+++ Implement strStr().py
@@ -1,18 +1,17 @@
 # https://leetcode.com/problems/implement-strstr/
-test = ""
-print(len(test))
 
 class Solution:
     def strStr(haystack: str, needle: str) -> int:
         if len(needle) == 0:
             return 0
-        for i in needle:
-            if needle in haystack:
-                return haystack.index(i)
-            else:
-                return -1
+        for i in range(len(haystack)):
+            print(i)
+            if needle[0] == haystack[i]:
+                if haystack[i: (i + len(needle))] == needle:
+                    
+                    return i
+        return -1
 
 
 s = Solution
 print(s.strStr("hello", "ll"))
-            
