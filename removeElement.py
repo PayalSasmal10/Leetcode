efc92900befc16528586503eacefc0373a8828a9
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-08 22:36:15 +0530

    remove element solved

--- removeElement.py
+++ removeElement.py
@@ -1,18 +1,11 @@
 # https://leetcode.com/problems/remove-element/
 
-test = [0,1,2,2,3,0,4,2]
-print("t:",test[7])
+
 class Solution:
     def removeElement(nums, val: int) -> int:
-        length = len(nums)
-        for i in range(length-1):
-            print("i:",i)
-            if nums[i] == val:
-                print(nums[i])
-                nums.remove(nums[i])
-                print(nums)
-                length = len(nums)
-                print("length:", length)
+        while val in nums:
+            nums.remove(val)
+        print(len(nums))
         
 s = Solution
 s.removeElement([0,1,2,2,3,0,4,2],2)
