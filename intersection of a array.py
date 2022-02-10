7fa9042bef1017df6776635a7390ed9319d43461
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-10 08:42:25 +0530

    other was solved intersection of array problem

--- intersection of a array.py
+++ intersection of a array.py
@@ -4,8 +4,18 @@ class Solution:
     def intersection(nums1, nums2):
         nums1_set = set(nums1)
         nums2_set = set(nums2)
-        new_result = nums1_set.intersection(nums2_set)
-        return list(new_result)
+        return list(nums1_set.intersection(nums2_set))
 
 s = Solution
 print(s.intersection([1,2,2,1],[2,2]))
+
+class Solution2:
+    def intersection2(nums1, nums2):
+        new_result = []
+        for i in nums1:
+            if i in nums2 and i not in new_result:
+                new_result.append(i)
+        return list(new_result)
+
+s2 = Solution2
+print(s2.intersection2([1,2,2,1],[2,2]))
