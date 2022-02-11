ac7fe2dedf2086f05488fd58ccad4c8f6dacb2ac
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-11 08:13:20 +0530

    remove element updated

--- removeElement.py
+++ removeElement.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/remove-element/
 
-# 1st way
+# 1st way- o(n^2) as remove function has O(n) time complexity
 class Solution:
     def removeElement(nums, val: int) -> int:
         while val in nums:
@@ -10,7 +10,7 @@ class Solution:
 s = Solution
 s.removeElement([0,1,2,2,3,0,4,2],2)
 
-# 2nd way
+# 2nd way - same way for this approach
 
 class Solution:
     def removeElement(nums, val: int) -> int:
