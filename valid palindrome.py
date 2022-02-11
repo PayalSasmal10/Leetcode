84c9a5fda4484607813a0fb1ad6df5b686dbf79d
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-11 15:00:35 +0530

    valid palindrome solved

--- valid palindrome.py
+++ valid palindrome.py
@@ -20,5 +20,28 @@ class Solution:
 
 s = Solution
 print(s.isPalindrome("A man, a plan, a canal: Panama"))
+
+# 2nd way:
+class Solution2:
+    
+    def alphanumeric(self, st):
+        if ((ord(st)>=48 and ord(st)<=57) or (ord(st) >= 65 and ord(st) <= 90) or (ord(st) >=97 and ord(st) <= 122)):
+            return True
+        return False
+    
+    def isPalindrome2(self,s: str) -> bool:
+        str1 = ""
+        str2 = ""
+        for i in s.lower():
+            if self.alphanumeric(i):
+                str1 = i + str1
+            if self.alphanumeric(i):
+                str2 += i
+        if str1 == str2:
+            return True
+        else:
+            return False
                 
-        
+s2 = Solution2
+test=s2.isPalindrome2("A man, a plan, a canal: Panama")
+print(test)
