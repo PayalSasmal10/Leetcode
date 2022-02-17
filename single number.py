d10d4c7939b015823af90791ace0edfdf7a9b6fc
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-17 17:12:47 +0530

    single number solved diff way

--- single number.py
+++ single number.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/single-number/
+# https://leetcode.com/problems/single-number-ii/
 
+# o(n^2) complexity, as count function time complexity is O(n) and it is inside for loops so two inner loop. so O(n^2)
 class Solution:
     def singleNumber(nums) -> int:
         for i in nums:
@@ -9,3 +11,21 @@ class Solution:
 
 s = Solution
 print(s.singleNumber([2,2,1]))
+
+
+# 2nd way:
+class Solution2:
+    def singleNumber2(nums) -> int:
+        test = {}
+        k= 1
+        for i,j in enumerate(nums):
+            if nums[i] in test:
+                test[nums[i]] = k+1
+            else:
+                test[nums[i]] = k
+        for m in test:
+            if test.get(m) == 1:
+                return m
+
+s2 = Solution2
+print(s2.singleNumber2([0,1,0,1,0,1,99]))
