7a1823df795ea7087e8d2c628e8434dacfea94e9
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-22 19:40:06 +0530

    Charecter Count of string

--- CharecterCountOfString.py
+++ CharecterCountOfString.py
@@ -1,14 +1,5 @@
 # how many charecter presents in my strings
 
-# from cv2 import add
-
-
-# dictt = {'p':1, 'a': 2, 'y': 1, 'l': 1}
-# j = 0
-# dictt.update({'t': 8})
-# dictt.in
-# print(dictt)
-
 str1 = "payal"
 test = {}
 countt = 0
