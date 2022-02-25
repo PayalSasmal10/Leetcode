a397afc9ffe2da1aeba728e6620c6bcb6483a362
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-25 13:11:03 +0530

    Two sum array 2 updated

--- Two Sum array 2.py
+++ Two Sum array 2.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
+# 1st way:
+
 class Solution:
     def twoSum(self, numbers, target):
         i = 0
@@ -10,8 +12,26 @@ class Solution:
             elif numbers[i] + numbers[j] < target:
                 i += 1
             elif numbers[i] + numbers[j] == target:
-                print("test")
-                return ([i, j])
+                return ([i+1, j+1])
 
 s = Solution()
 print(s.twoSum([2,7,11,15], 9))
+
+# 2md way
+class Solution1:
+    def twoSum1(self, numbers, target: int):
+        dictt = {}
+        for i in range(len(numbers)):
+            if target - numbers[i] in dictt:
+                
+                return [dictt[target - numbers[i]], i+1]
+                
+            else:
+                dictt[numbers[i]] = i+1
+
+
+s1 = Solution1()
+print(s1.twoSum1([2,7,11,15],9))
+                
+        
+        
