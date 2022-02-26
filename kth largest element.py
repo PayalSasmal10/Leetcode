5ad6a929846279b77320058645380f5716157624
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-26 13:20:56 +0530

    kth largest element updated

---
+++ kth largest element.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/kth-largest-element-in-an-array/
+from heapq import nlargest
+
+
+class Solution:
+    def findKthLargest(self, nums, k: int) -> int:
+       print(nlargest(k, nums))
+
+s = Solution()
+s.findKthLargest([3,2,3,1,2,4,5,5,6], 4)
+
+
