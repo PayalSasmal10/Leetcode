105ca44291c5984b3eef467bf9b25924a7485a7b
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-27 18:13:30 +0530

    find peak element added hard level of peak index

--- find peak element.py
+++ find peak element.py
@@ -11,3 +11,20 @@ class Solution:
 
 s = Solution()
 print(s.findPeakElement([1,2,1,3,5,6,4]))
+
+
+# 2nd way as Binary search
+class Solution1:
+    def findPeakElement(self, nums) -> int:
+        s, e = 0, len(nums) -1
+
+        while(s<e):
+            mid = s + (e -s) // 2
+            if nums[mid] < nums[mid + 1]:
+                s = mid + 1
+            else:
+                e = mid
+        return s
+
+s1 = Solution1()
+print(s1.findPeakElement([1,2,1,3,5,6,4]))
