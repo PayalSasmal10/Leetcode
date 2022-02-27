ba96ee27fca0f8a35cf92b7a2e7b9cd3eb040f5a
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-27 18:04:12 +0530

    peak index in mountain array

--- peak index in mountain array.py
+++ peak index in mountain array.py
@@ -10,3 +10,21 @@ class Solution:
         return arr.index(max_element)
         
 
+
+
+# binary solution O(n)
+
+class Solution1:
+    def peakIndexInMountainArray(self, arr) -> int:
+        s = 0
+        e = len(arr) - 1
+        while(s<e):
+            mid = s + (e-s) // 2
+            if arr[mid] < arr[mid+1]:
+                s = mid+1
+            else:
+                e = mid
+        return s 
+
+s1 = Solution1()
+print(s1.peakIndexInMountainArray([0,2,1,0]))
