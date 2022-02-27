05383e1d63ee507fe4e13bc2a18ecf112a5016ad
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-27 13:39:16 +0530

    container with most water updated with two pointer solution

--- container with most water.py
+++ container with most water.py
@@ -7,11 +7,27 @@ class Solution:
         for i in range(len(height) - 1):
             for j in range(i+1, len(height)):
                 currentArea = min(height[i], height[j])*(j-i)
-                print("i :", i, "j:", j)
-                print("currentarea:",currentArea)
                 area = max(area,currentArea)
         return area
         
 
 s = Solution()
+print(s.maxArea([1,8,6,2,5,4,8,3,7]))
+
+# Two pointer solution with O(n) time complexity and O(1) space complexity
+class Solution1:
+    def maxArea(self, height) -> int:
+        area = 0
+        s = 0
+        e = len(height) - 1
+        while(s<e):
+            area = max(area, min(height[e], height[s])*(e-s))
+            if height[s] < height[e]:
+                s += 1
+            else:
+                e -= 1
+        return area
+        
+
+s1 = Solution1()
 print(s.maxArea([1,8,6,2,5,4,8,3,7]))
