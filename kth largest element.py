a22788c5be910552424bd147b15de3d39e9ab1db
Payal Sasmal <sasmalpayal@gmail.com>
2022-02-27 12:07:21 +0530

    updated kth largest element

--- kth largest element.py
+++ kth largest element.py
@@ -4,7 +4,7 @@ from heapq import nlargest
 
 class Solution:
     def findKthLargest(self, nums, k: int) -> int:
-       print(nlargest(k, nums))
+       print(nlargest(k, nums)[-1])
 
 s = Solution()
 s.findKthLargest([3,2,3,1,2,4,5,5,6], 4)
