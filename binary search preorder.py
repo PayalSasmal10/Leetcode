aa9170aef951d535ac3de03103cbef9dfe3950ce
Payal Sasmal <sasmalpayal@gmail.com>
2022-03-05 00:09:02 +0530

    binary search preorder solved

--- binary search preorder.py
+++ binary search preorder.py
@@ -1,11 +1,12 @@
+#https://leetcode.com/problems/binary-tree-preorder-traversal/
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def preorderTraversal(self, root: TreeNode):
         self.result = []
         def Preorder(node):
             if node == None:
