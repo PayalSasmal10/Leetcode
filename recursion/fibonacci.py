ea5913131e466aeaa56919a75314bf5f3a23581e
Payal Sasmal <sasmalpayal@gmail.com>
2022-03-06 22:04:09 +0530

    fibonacci in top down approach

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -1,4 +1,5 @@
 # 0 1 1 2 3 5 8 13 ....
+# time complexity is O(2^n)
 def fibonacci(n):
     if n in [0,1]:
         return n
@@ -8,4 +9,15 @@ def fibonacci(n):
 print(fibonacci(4))
 
 
+# Let's use Top down approach
+def fibonacci1(n,dp):
+    
+    if n in [0,1]:
+        return n
+    if dp[n] is None:
+        dp[n] = fibonacci1(n-1,dp)+fibonacci1(n-2,dp)
+    return dp[n]
+
+dp = [None] *101
+print(fibonacci1(4,dp))
 
