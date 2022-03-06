caaf8a3bce4f31bbbb8cf44c30af06ca22284ff3
Payal Sasmal <sasmalpayal@gmail.com>
2022-03-06 10:26:35 +0530

    stock selling part 2 solved

---
+++ stockselling 2.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+class Solution:
+    def maxProfit(self, prices) -> int:
+        current_value= prices[0]
+        profit = 0
+        for i in range(len(prices)):
+            if current_value < prices[i]:
+                profit += prices[i]-current_value
+
+            current_value = prices[i]
+                
+        return profit
+        
+s = Solution()
+print(s.maxProfit([7,1,5,3,6,4]))
