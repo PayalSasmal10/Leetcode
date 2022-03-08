a25db009517be74996cda25590c4f5e7d489a476
Payal Sasmal <sasmalpayal@gmail.com>
2022-03-08 22:37:00 +0530

    search matrix solved

--- search a 2D matrix.py
+++ search a 2D matrix.py
@@ -2,27 +2,21 @@
 
 class Solution:
     def searchMatrix(self, matrix, target: int) -> bool:
-        i = 0
         s = 0
-        e = len(matrix) - 1
+        e = len(matrix[s]) - 1
         
-        while(i<len(matrix)):
-            j = len(matrix[i])
-            print("j:", j)
-            if j > 0:
-                mid = (s+j) // 2
-                if matrix[i][mid] == target:
-                    return mid
-                elif matrix[i][mid] < target:
-                    s += 1
-                    j -= 1
-                else:
-                    e -= 1
-                    j -= 1
-            else:
-                i += 1
+        while(s<len(matrix) and e >=0):
+            
+            if matrix[s][e] == target:
+                return True
+            elif matrix[s][e] < target:
+                s += 1
                 
-        return -1
+            else:
+                e -= 1
+                 
+                
+        return False
         
 s = Solution()
 print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],13))
