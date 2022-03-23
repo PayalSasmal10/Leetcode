cec6836797240a321dd31d94a3fab2f815bf5672
Payal Sasmal <sasmalpayal@gmail.com>
2022-03-23 22:23:55 +0530

    finding sub strings added

--- substring/find all substrings.py
+++ substring/find all substrings.py
@@ -1,4 +1,6 @@
 # o(n^2) time complexity
+
+strings = "payal"
 strings = "payal"
 sub_str = []
 class subStrings:
@@ -14,5 +16,34 @@ class subStrings:
 s = subStrings()
 s.substring(strings)
     
+#p pa pay paya payal 
+strings = "payal"
+print("here",strings[:-1:])
+
+# o(n) time complexity and space complexity
+class substrings:
+    def substrings_func(self,strings):
+        k = 0
+        l = k
+        new_list = []
+        test = ""
+        while(k<len(strings)):
+            print(k)
+            if l<len(strings):
+                test += strings[l]
+                new_list.append(test)
+                l += 1
+            if l >= len(strings):
+                k += 1
+                l = k
+                test = ""
+        print(new_list)
+
+s1 = substrings()
+s1.substrings_func(strings)
+
+
+
+
 
     
