3ac0e319df86d6131197eda4068f093839fe3148
Payal Sasmal <sasmalpayal@gmail.com>
2022-04-21 23:06:37 +0530

    all 3 sub arrays sum

--- substring_or_sliding_window/all 3 sub arrays sum.py
+++ substring_or_sliding_window/all 3 sub arrays sum.py
@@ -1,12 +1,12 @@
+# o(n^2) time complexity
 class subArrays:
     def sub_array(self,strings, k=3):
         i = j = 0
         sum = 0
         max_val = strings[0]
         new_list = []
-        new_list1 = []
-        for i in range(len(strings)-3):
-            for j in range(i, i+3):
+        for i in range(len(strings)-k):
+            for j in range(i, i+k):
                 new_list.append(strings[j])
                 sum += strings[j]
                 max_val = max(max_val,sum)
@@ -22,3 +22,29 @@ strings = [2, 3, 5, 2, 9, 7, 1]
 
 s = subArrays()
 print(s.sub_array(strings))
+print("...............................")
+# o(n) time complexity
+class subArrays1:
+    def sub_array1(self,strings, k=3):
+        i = j = 0
+        sum = 0
+        max_val = strings[0]
+        new_list = []
+        while(i<len(strings)-k):
+            if j < i+k:
+                sum += strings[j]
+                max_val = max(sum, max_val)
+                new_list.append(strings[j])
+                j += 1
+            elif j <= i+k:
+                i += 1
+                j = i
+                print(new_list)
+                sum = 0
+                new_list = []
+
+        return max_val
+
+
+sa = subArrays1()
+print(sa.sub_array1(strings))
